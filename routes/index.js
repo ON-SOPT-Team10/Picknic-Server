2be59fac117759c2673a3f7b664c9e4e97970552
KangJunewoo <brianjune.dev@gmail.com>
2020-12-05 15:52:54 +0900

    [UPDATE] changing express settings

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use('/profile', require('./profile'));
+router.use('/main', require('./main'));
+router.use('/bookmark', require('./bookmark'));
 
 module.exports = router;
---
+++ routes/profile.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+//작업
+
+module.exports = router;
