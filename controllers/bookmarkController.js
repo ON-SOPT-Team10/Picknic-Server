c2a1f1ff6946cbfd18cd58addc2067c46820c4b5
KangJunewoo <brianjune.dev@gmail.com>
2020-12-17 03:47:55 +0900

    [UPDATE] 북마크 토글할때 bookmarkCount 변화 반영

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -6,18 +6,29 @@ const {Feed} = require('../models');
 module.exports = {
     toggleBookmark: async(req,res) => {
         const feedId = req.params.feedId;
-        if (!feedId)
-            return res.status(sc.BAD_REQUEST).
-                send(ut.fail(sc.BAD_REQUEST, rm.WRONG_INDEX));
+        if (!feedId) return res.status(sc.BAD_REQUEST) // feedId를 클라에서 넘겨주지 않은 경우.
+            .send(ut.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
         
         try{
-            // 먼저 해당 북마크를 찾은 다음에
-            const feed = await Feed.findOne({where:{id:feedId}});
-
-            // 토글진행
-            const result = await Feed.update({isBookmarked:!feed.isBookmarked},{where:{id:feedId}});
-            return res.status(sc.OK)
-                .send(ut.success(sc.OK, rm.SUCCESS, result));
+            const feed = await Feed.findOne({where:{id:feedId}}); // 해당 피드를 먼저 찾은 다음에
+            if(!feed) return res.status(sc.BAD_REQUEST) // 없는 인덱스면 에러 띄워주고
+                .send(ut.fail(sc.BAD_REQUEST, rm.WRONG_INDEX));
+            
+            if(feed.isBookmarked){ // 토글 진행
+                result = await Feed.update({
+                    isBookmarked:false,
+                    bookmarkCount:feed.bookmarkCount-1
+                },{where:{id:feedId}});
+            } else{
+                result = await Feed.update({
+                    isBookmarked:true,
+                    bookmarkCount:feed.bookmarkCount+1
+                },{where:{id:feedId}});
+            }
+            
+            return res.status(sc.OK) // 성공메세지 반환
+                .send(ut.success(sc.OK, rm.SUCCESS, "토글 성공"));
+                
         } catch(err){
             console.error(err);
             return res.status(sc.INTERNAL_SERVER_ERROR)
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -10,9 +10,9 @@ const {
 module.exports = {
     getStoriesFeeds: async(req,res)=>{
         try{
-            const stories = await Story.findAll();
-            const feeds = await Feed.findAll();
-            return res.status(sc.OK)
+            const stories = await Story.findAll(); // 스토리 모두 불러오고
+            const feeds = await Feed.findAll(); // 피드 모두 불러온 뒤
+            return res.status(sc.OK) // 합쳐서 리턴
                 .send(ut.success(sc.OK, rm.SUCCESS, {stories:stories,feeds:feeds}));
         } catch(err){
             console.error(err);
